a2c47739335df97404323c432ac0caefcb906136
Petter Bomban <79021183+petter-bomban@users.noreply.github.com>
2021-03-12 18:01:36 +0100

    new ascii art

--- helpers.ps1
+++ helpers.ps1
@@ -15,19 +15,14 @@ function Confirm-IfWhitespace ($str) {
 function Get-ASCIIText {
 
     $AsciiArt = @'
-     _                                             
-    / \    _____   _ _ __ ___                      
-   / _ \  |_  / | | | '__/ _ \                     
-  / ___ \  / /| |_| | | |  __/                     
- /_/_  \_\/___|\__,_|_|  \___|     _   _           
- |_ _|_ __ | |_ ___ _ __ __ _  ___| |_(_)_   _____ 
-  | || '_ \| __/ _ \ '__/ _` |/ __| __| \ \ / / _ \
-  | || | | | ||  __/ | | (_| | (__| |_| |\ V /  __/
- |___|_| |_|\__\___|_|  \__,_|\___|\__|_| \_/ \___|
- |  \/  | ___ _ __  _   _                          
- | |\/| |/ _ \ '_ \| | | |                         
- | |  | |  __/ | | | |_| |                         
- |_|  |_|\___|_| |_|\__,_|                         
+     ___  ________  ___
+    / _ \|_   _|  \/  |
+   / /_\ \ | | | .  . |
+   |  _  | | | | |\/| |
+   | | | |_| |_| |  | |
+   \_| |_/\___/\_|  |_/  
+  Azure Interactive Menu
+           v0
 
 '@
 
@@ -38,6 +33,5 @@ function Get-ASCIIText {
 function Write-ObjectToHost ($object) {
 
     $object = $object | Format-List | Out-String
-    Write-Host "
-    $object"
+    Write-Host "$object"
 }
