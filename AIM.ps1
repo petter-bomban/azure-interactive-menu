28a0d4b3f06f73c9a55a77b6f771077169e3d189
Petter Bomban <79021183+petter-bomban@users.noreply.github.com>
2021-03-14 18:57:08 +0100

    Start work on maintenance menu

--- AIM.ps1
+++ AIM.ps1
@@ -20,10 +20,6 @@ function Start-AIM {
 }
 
 ## Start session log
-$LogFolder = Join-Path -Path $PSScriptRoot -ChildPath "session_logs"
-$LogName   = "AIM-Log_$(Get-Date -Format 'yyyy-MM-dd_hh-mm-ss').txt"
-$FullLogPath = Join-Path -Path $LogFolder -ChildPath $LogName
-
 Start-Transcript -Path $FullLogPath -NoClobber
 
 ## Start main application
--- global_variables.ps1
+++ global_variables.ps1
@@ -2,3 +2,7 @@
 
 $Global:session = ""
 
+$LogFolder = Join-Path -Path $PSScriptRoot -ChildPath "session_logs"
+$LogName   = "AIM-Log_$(Get-Date -Format 'yyyy-MM-dd_hh-mm-ss').txt"
+
+$Global:FullLogPath = Join-Path -Path $LogFolder -ChildPath $LogName
